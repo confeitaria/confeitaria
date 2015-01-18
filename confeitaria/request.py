4671f7b871a52d6c95b2d65a8d3f39c6a316f9d4
Adam Brandizzi <adam.brandizzi@liferay.com>
2015-01-18 20:44:40 -0300

    #31 Turning requet objects into a "tuple"

--- confeitaria/request.py
+++ confeitaria/request.py
@@ -3,6 +3,13 @@ A request object should contain the info relevant about the HTTP request.
 """
 
 class Request(object):
-    def __init__(self, query_parameters=None):
+    def __init__(self, page=None, args=None, kwargs=None,
+            query_parameters=None):
         self.query_parameters = {} if not query_parameters else query_parameters
+        self.page = page
+        self.args = args
+        self.kwargs = kwargs
+        self.tuple = (page, args, kwargs)
 
+    def __getitem__(self, key):
+        return self.tuple[key]
