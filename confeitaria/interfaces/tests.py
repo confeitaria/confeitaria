e3d5f4ece9dacff40ee87d8755e00ff00781a4e5
Adam Brandizzi <adam.brandizzi@liferay.com>
2015-05-25 05:59:24 -0300

    #40 / #28 Test for returning False when set_url(0 is no method

--- confeitaria/interfaces/tests.py
+++ confeitaria/interfaces/tests.py
@@ -26,6 +26,18 @@ class TestURLedPage(unittest.TestCase):
 
         self.assertTrue(interfaces.has_set_url(TestPage()))
 
+    def test_has_set_url_is_false_no_method(self):
+        """
+        This tests ensures the ``has_set_url()`` returns ``False`` if its
+        argument has a ``set_url()`` method accepting one mandatory argument.
+        """
+        class TestPage(object):
+            def __init__(self):
+                self.set_url = 'string'
+
+        self.assertFalse(interfaces.has_set_url(TestPage()))
+
+
 test_suite = unittest.TestLoader().loadTestsFromTestCase(TestURLedPage)
 test_suite.addTest(doctest.DocTestSuite(interfaces))
 
