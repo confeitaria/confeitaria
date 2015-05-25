c5bc18946a220fac08bdbb2de950ded225be99d1
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-05-24 23:07:02 -0300

    #40 A separated test for URLedPage was created

--- confeitaria/interfaces/tests.py
+++ confeitaria/interfaces/tests.py
@@ -3,7 +3,19 @@ import unittest
 
 import interfaces
 
-test_suite = unittest.TestSuite()
+class TestURLedPage(unittest.TestCase):
+
+    def test_set_url_to_get_url(self):
+        """
+        This test ensures that what goes into ``URLedPage.set_url()`` is
+        retrieved by ``URLedPage.get_url()``.
+        """
+        page = interfaces.URLedPage()
+        page.set_url('/test')
+
+        self.assertEqual('/test', page.get_url())
+
+test_suite = unittest.TestLoader().loadTestsFromTestCase(TestURLedPage)
 test_suite.addTest(doctest.DocTestSuite(interfaces))
 
 def load_tests(loader, tests, ignore):
