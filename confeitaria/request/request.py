0727f752fbb61d5e3a269f1c46b52e49799443f3
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-06-09 14:44:36 -0300

    #67 Using None as default values to avoid "growing empty" lists/dicts

--- confeitaria/request/request.py
+++ confeitaria/request/request.py
@@ -4,12 +4,12 @@ A request object should contain the info relevant about the HTTP request.
 
 class Request(object):
     def __init__(
-            self, page=None, path_args=[], query_args={},
-            form_args={}, args=[], kwargs=[]
+            self, page=None, path_args=None, query_args=None, form_args=None,
+            args=None, kwargs=None
         ):
         self.page = page
-        self.path_args = path_args
-        self.query_args = query_args
-        self.form_args = form_args
-        self.args = args
-        self.kwargs = kwargs
+        self.path_args = path_args if path_args is not None else []
+        self.query_args = query_args if query_args is not None else {}
+        self.form_args = form_args if form_args is not None else {}
+        self.args = args if args is not None else []
+        self.kwargs = kwargs if kwargs is not None else {}
