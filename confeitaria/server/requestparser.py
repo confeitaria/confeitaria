372b1cf435f7ec96b7eead9f0e64aa2a4ba9e374
Adam Brandizzi <adam.brandizzi@liferay.com>
2015-06-10 23:08:32 -0300

    #67 A more or less consistent review of the documentation

--- confeitaria/server/requestparser.py
+++ confeitaria/server/requestparser.py
@@ -6,141 +6,214 @@ import confeitaria.responses
 
 class RequestParser(object):
     """
-    ``RequestParser`` is an implemetation of the URL parser protocol.
+    ``RequestParser`` is responsible for building a ``Request`` object from a
+    URL path and query string and, optionally, the body of a POST request.
 
-    The request parser protocol
-    ---------------------------
+    When initialized, the request parser will receive a page - very likely one
+    with subpages.
 
-    Request parsers are objects with a ``parse_request()`` method. This method
-    should receive an URL path as an argument and return a request object. This
-    request object should give access to at least five attributes:
+    The magic happens mostly when calling the ``parse_request()`` method. It
+    recieves as its argument an URL and the body of a POST request.
 
-    * ``page``: a page object;
-    * ``path_args``: a list of strings that are the components from the URL
-      path not included in the path to the page object;
-    * ``query_args``: a dictionary containing the parsed query paramenters;
-    *``form_args``: a dictionary containing the parsed body of a POST
-      request;
-    * ``args``: a list of strings be used as the argument to the index or action
-      method; and
-    * ``kwargs``: a dictionary, whose values can be string or ``None``,
-      containing optional arguments to the index or action method::
+    As a result, it returns a ``Request`` object, which for its turn has the
+    page pointed by the given path - if any - as well as relevant information
+    about how to call it. If no page could be found, then a
+    ``confeitaria.responses.NotFound`` response is thrown.
 
-        >>> # RequestParser requires such a page to work
-        >>> class TestPage(object):
-        ...     def index(self, arg, kwarg=None):
-        ...         return ''
-        >>> request_parser = RequestParser(TestPage())
-        >>> request = request_parser.parse_request('/v')
-        >>> hasattr(request.page, "index")
-        True
-        >>> isinstance(request.path_args, list)
-        True
-        >>> isinstance(request.query_args, dict)
-        True
-        >>> isinstance(request.form_args, dict)
-        True
-        >>> isinstance(request.args, list)
-        True
-        >>> isinstance(request.kwargs, dict)
-        True
+    The Object Publisher pattern
+    ----------------------------
+    ``RequestParser.parse_request()`` returns an stance of
+    ``confeitaria.request.Request``. This object has many attributes, and one
+    of the most important is ``page``. It is the page object that is pointed by
+    the given URL path.
 
-    It is mandatory that the page index or action method should be called having
-    the args list expanded to fill its mandatory arguments, as well as the
-    kwargs dict expanded to fill its optional arguments:
 
-        >>> request.page.index(*request.args, **request.kwargs)
-        ''
-
-    The ``parse_request()`` method can eventually also throw exceptions
-    representing HTTP status codes.
-
-    Parsing POST requests
-    ---------------------
-
-    ``parse_request()`` can also receive a second argument, which should be the
-    body of a POST request. It is from this argument that the values at
-    ``form_args`` will come from::
-
-        >>> class ActionPage(object):
-        ...     def action(self, kwarg=None):
-        ...         return 'kwarg is ' + kwarg
-        >>> request_parser = RequestParser(ActionPage())
-        >>> request = request_parser.parse_request('', 'kwarg=example')
-        >>> hasattr(request.page, "action")
-        True
-        >>> isinstance(request.path_args, list)
-        True
-        >>> isinstance(request.form_args, dict)
-        True
-
-    The ``RequestParser`` implemetation
-    -----------------------------------
-
-    ``RequestParser`` implements the request parser protocol mapping URLs to a
-    tree of objects, following the so called *object publisher* pattern.
+    ``RequestParser`` implements the so called *object publisher* pattern, where
+    URLs are addresses for real Python objects.
 
     Suppose we have the following page classes::
 
-        >>> class RootPage(object):
-        ...     def index(self):
-        ...         return 'Welcome to the ROOT PAGE'
-        >>> class SubPage(object):
-        ...     def index(self):
-        ...         return 'Welcome to the SUBPAGE'
-        >>> class SubSubPage(object):
-        ...     def index(self):
-        ...         return 'Welcome to the SUBPAGE of the SUBPAGE'
+    >>> class RootPage(object):
+    ...     def index(self):
+    ...         return 'root'
+    >>> class SubPage(object):
+    ...     def index(self):
+    ...         return 'sub'
+    >>> class AnotherPage(object):
+    ...     def index(self):
+    ...         return 'another'
+    >>> class ArgPage(object):
+    ...     def index(self, arg, kwarg='0'):
+    ...         return 'arg: {0} kwarg: {1}'.format(arg, kwarg)
+    >>> class KwArgPage(object):
+    ...     def index(self, kwarg1='1', kwarg2='2'):
+    ...         return 'kwarg1: {0} kwarg2: {1}'.format(kwarg1, kwarg2)
+    >>> class ActionPage(object):
+    ...     def action(self, kwarg1='1', kwarg2='2'):
+    ...         pass
 
     Then, we build the following object with them::
 
-        >>> page = RootPage()
-        >>> page.sub = SubPage()
-        >>> page.sub.another = SubSubPage()
+    >>> root = RootPage()
+    >>> root.attr = object()
+    >>> root.sub = SubPage()
+    >>> root.sub.another = AnotherPage()
+    >>> root.arg = ArgPage()
+    >>> root.kwarg = KwArgPage()
+    >>> root.action = ActionPage()
 
-    We can build ``RequestParser`` passing it as an argument::
+    And then create a request parser as the following::
 
-        >>> request_parser = RequestParser(page)
+    >>> parser = RequestParser(root)
 
     ...and now URL paths should be mapped to the pages of the object. The root
     path is mapped to the root page::
 
-        >>> request = request_parser.parse_request('/')
-        >>> request.page == page
-        True
+        >>> page = parser.parse_request('/').page
+        >>> page.index()
+        'root'
 
     If the path has one more compoment, ``RequestParser`` tries to get a page
     from the attribute (of the root page) with the same name of the path
     component::
 
-        >>> request = request_parser.parse_request('/sub')
-        >>> request.page == page.sub
-        True
+        >>> page = parser.parse_request('/sub').page
+        >>> page.index()
+        'sub'
 
     If the path has yet another component, then the request parser tries to get
     an attribute from the previous subpage, and so on::
 
-        >>> request = request_parser.parse_request('/sub/another')
-        >>> request.page == page.sub.another
+        >>> page = parser.parse_request('/sub/another').page
+        >>> page.index()
+        'another'
+
+    The ``Request`` object
+    ----------------------
+
+    We have seen how the returned object has a ``page`` attribute, but the
+    ``confeitaria.request.Request`` object should have at least six
+    attributes. They are::
+
+    ``page``
+        The page object pointed by the given URL path, if any. It is either the
+        page given to the ``RequestParser`` constructor or one of its subpages::
+
+        >>> isinstance(parser.parse_request('/').page, RootPage)
         True
+        >>> isinstance(parser.parse_request('/sub').page, SubPage)
+        True
+
+    ``path_args``
+        The components o the URL path do not necessarily point only to the page.
+        If the page method has mandatory arguments, there should be extra
+        components in the URL path after a page is found. These extra componets
+        will fill the mandatory arguments from the page method from the found
+        page. You can find these components are found at the``path_args``
+        attribute from the request::
+
+        >>> parser.parse_request('/arg/value').path_args
+        ['value']
+
+        If there is no extra compoment, it will be an empty list::
+
+        >>> parser.parse_request('/sub').path_args
+        []
+
+    ``query_args``
+        A dict containing _all_ values from the query string from the URL.
+
+        >>> parser.parse_request('/?arg=value').query_args
+        {'arg': 'value'}
+        >>> parser.parse_request('/sub?arg=value').query_args
+        {'arg': 'value'}
+        >>> parser.parse_request('/sub?arg=value&kwarg1=ok').query_args
+        {'kwarg1': 'ok', 'arg': 'value'}
+
+        If the query string is empty, so is the attribute
+
+        >>> parser.parse_request('/sub').query_args
+        {}
+
+    ``form_args``
+        A dict containing _all_ values from the request body passed as the
+        second argumetn from ``RequestParser.parse_request()``::
+
+        >>> parser.parse_request('/action?arg1=value', 'arg2=ok').form_args
+        {'arg2': 'ok'}
+
+        If none is given, it is an empty dict::
+
+        >>> parser.parse_request('/sub?arg1=value').form_args
+        {}
+
+    ``args``
+        A sublist of ``path_args`` to be unpacked as the positional arguments of
+        the page method from ``page``::
+
+        >>> parser.parse_request('/arg/value').args
+        ['value']
+
+        In practice, right now, in this implementation, it will be equal to
+        ``path_args`` but some undefined  behaviors can change in the future
+        changing this fact.
+
+    ``kwargs``
+        A dict to be unpacked as the keyword arguments of ``page`` page method.
+        If no body argument is given, its values will come from ``query_args``::
+
+        >>> parser.parse_request('/kwarg?kwarg1=query').kwargs
+        {'kwarg1': 'query'}
+
+        If the body argumet is given, then its values will come from
+        ``form_args``::
+
+        >>> parser.parse_request('/action?kwarg1=query', 'kwarg1=form').kwargs
+        {'kwarg1': 'form'}
+
+        This attribute will only contain values that matches optional arguments
+        from the page method to be called - other values from ``form_args`` and
+        ``query_args`` are not present::
+
+        >>> parser.parse_request('/kwarg?kwarg1=query&nothere=true').kwargs
+        {'kwarg1': 'query'}
+        >>> parser.parse_request('/action', 'kwarg1=form&nothere=true').kwargs
+        {'kwarg1': 'form'}
+
+        Also, it does not contain values for positional arguments, which always
+        should come from the URL path components::
+
+        >>> r = parser.parse_request('/arg/yes?kwarg=query&arg=no')
+        >>> r.kwargs
+        {'kwarg': 'query'}
+        >>> r.args
+        ['yes']
+
+    Unpacking ``args`` and ``kwargs`` as the arguments to the page method of the
+    provided page should always match::
+
+        >>> r.page.index(*r.args, **r.kwargs)
+        'arg: yes kwarg: query'
+
+    Raisig ``NotFound``
+    -------------------
 
     On the other hand, if some of the pages do not have an attribute with the
     same name as the next compoment, then an ``confeitaria.responses.NotFound``
     exception is raised to signalize that the page was not found::
 
-        >>> request_parser.parse_request('/nopage')
+        >>> parser.parse_request('/nopage')
         Traceback (most recent call last):
           ...
         NotFound: /nopage not found
-        >>> request_parser.parse_request('/sub/nopage')
+        >>> parser.parse_request('/sub/nopage')
         Traceback (most recent call last):
           ...
-        NotFound: /nopage not found
+        NotFound: /sub/nopage not found
 
     The same happens when an attribute is found but it is not a page::
 
-        >>> page.attr = object()
-        >>> request_parser.parse_request('/attr')
+        >>> parser.parse_request('/attr')
         Traceback (most recent call last):
           ...
         NotFound: /attr not found
@@ -154,70 +227,44 @@ class RequestParser(object):
     arguments, and the path has the same number of compoments remaining as the
     number of arguments of the page method.
 
-    An example can make it clearer. Consider the following page::
+    For example, the index method from ``ArgPage`` expects a positional
+    argument. So, any URL hitting the ``ArgPage`` object should have an extra
+    component, which will be an argument to the index method::
 
-        >>> class HelloPage(object):
-        ...     def index(self, who):
-        ...         return 'Hello {0}'.format(who)
-        >>> page = HelloPage()
-        >>> request_parser = RequestParser(page)
-
-    Since the ``index()`` method expects parameters, we can pass one more
+    Since the ``index()`` method expects parameters, we should pass one more
     component in the path::
 
-        >>> request = request_parser.parse_request('/world')
-        >>> request.page == page
-        True
-        >>> request.path_args
-        ['world']
+        >>> request = parser.parse_request('/arg/value')
         >>> request.page.index(*request.args)
-        'Hello world'
+        'arg: value kwarg: 0'
 
     Yet, we cannot pass more path compoments than the number of arguments in the
     method::
 
-        >>> request_parser.parse_request('/world/again')
+        >>> parser.parse_request('/arg/value/other')
         Traceback (most recent call last):
           ...
-        NotFound: /world/again not found
+        NotFound: /arg/value/other not found
 
-    Neither can we pass _less_ arguments - ti will also result in a
-    ``confeitaria.responses.NotFound`` exception::
+    Neither can we pass _less_ arguments - it will also result in a
+    ``confeitaria.responses.NotFound`` response::
 
     ::
-        >>> request = request_parser.parse_request('/')
+        >>> parser.parse_request('/arg')
         Traceback (most recent call last):
           ...
-        NotFound: / not found
+        NotFound: /arg not found
 
     Optional parameters
     -------------------
 
     Index methods can also have optional arguments. They can come from either
-    the query parameters or from the submitted form fields. If the latter is
-    given, then
+    the query string or from the submitted form fields. If the request body is
+    not given, then the optional arguments will come from the query string::
 
-    Given for instance the page below::
 
-        >>> class HelloPage(object):
-        ...     def index(self, greeting='Hello', greeted='World'):
-        ...         return '{0} {1}'.format(greeting, greeted)
-        >>> page = HelloPage()
-        >>> request_parser = RequestParser(page)
-
-    ...``RequestParser`` will behave this way::
-
-        >>> request = request_parser.parse_request(
-        ...     '/?greeting=Hi&greeted=Earth'
-        ... )
-        >>> request.page == page
-        True
-        >>> request.path_args
-        []
-        >>> request.query_args
-        {'greeting': 'Hi', 'greeted': 'Earth'}
-        >>> request.page.index(*request.path_args, **request.kwargs)
-        'Hi Earth'
+    >>> parser.parse_request('/kwarg?kwarg1=value').kwargs
+    {'kwarg1': 'value'}
 
     Action methods, on the other hand, are not expected to use the query
     arguments as their optional parameters. Instead, it should use the parsed
@@ -227,20 +274,8 @@ class RequestParser(object):
     the body of such a POST request, as a string. The parsed values can be found
     at the ``form_args`` attribute from the request::
 
-        >>> class ActionPage(object):
-        ...     def action(self, kwarg=None):
-        ...         return ''
-        >>> page = ActionPage()
-        >>> request_parser = RequestParser(page)
-        >>> request = request_parser.parse_request('', 'kwarg=example')
-        >>> request.page == page
-        True
-        >>> request.path_args
-        []
-        >>> request.query_args
-        {}
-        >>> request.form_args
-        {'kwarg': 'example'}
+    >>> parser.parse_request('/action?kwarg1=query', 'kwarg1=form').kwargs
+    {'kwarg1': 'form'}
     """
 
     def __init__(self, page):
