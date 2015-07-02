9de306c44c9632203e1517395f6e9ed0f3be773c
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-07-02 00:57:19 -0300

    #93 Killig last time.wait()! Tests yet faster!

--- confeitaria/server/tests/server.py
+++ confeitaria/server/tests/server.py
@@ -6,6 +6,7 @@ import requests
 
 from confeitaria.reference.tests import TestReference
 from ..server import Server, get_cookies_tuples, replace_none_location
+from ..waiters import wait_server_up, wait_server_down
 from confeitaria.server import server
 from confeitaria import runner
 
@@ -27,7 +28,7 @@ class TestServer(TestReference):
 
         process = multiprocessing.Process(target=server.run)
         process.start()
-        time.sleep(1)
+        wait_server_up('', 8000)
 
         request = requests.get('http://localhost:8000/')
         self.assertEquals('page content', request.text)
@@ -35,6 +36,7 @@ class TestServer(TestReference):
         self.assertEquals('text/html', request.headers['content-type'])
 
         process.terminate()
+        wait_server_down('', 8000)
 
     def test_with(self):
         """
