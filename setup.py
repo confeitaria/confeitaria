e32a7220b4103043bc164b1ed0688966bf4c9e2b
Adam Brandizzi <adam.brandizzi@liferay.com>
2015-07-02 10:21:17 -0300

    #77 Let's just do it and move on...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="confeitaria",
-    version="0.1.dev0",
+    version="0.1.a1",
     author='Adam Victor Brandizzi',
     author_email='adam@brandizzi.com.br',
     description='Confeitaria is a Python web framework',
