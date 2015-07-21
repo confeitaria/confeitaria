b3e583cfee3f3f6656e771e202fc3fc968f7b0a9
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-07-20 22:41:54 -0300

    #50 Documentation to SessionStorage()

--- confeitaria/server/session.py
+++ confeitaria/server/session.py
@@ -1,4 +1,27 @@
 class SessionStorage(object):
+    """
+    ``SessionStorage`` stores and retrieves sessions used by the server. This
+    class stores the sessions in memory as actual Python dicts.
+
+    Any session storage should return a new session if a not yet set key is
+    requested::
+
+    >>> s = SessionStorage()
+    >>> 'key' in s
+    False
+    >>> s['key']
+    {}
+    >>> 'key' in s
+    True
+
+    Naturally, once a session is created, it should always be retrievable from
+    its key::
+
+    >>> session = s['id']
+    >>> session['value'] = 'example'
+    >>> s['id']['value']
+    'example'
+    """
     def __init__(self):
         self.sessions = {}
 
